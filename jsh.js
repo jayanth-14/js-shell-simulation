26564832f6795b802b3550b3347d41b922176f0b
jayanth-14 <jayanthpetchetii@gmail.com>
2025-11-05 21:59:21 +0530

    implemented ls and array methods

--- jsh.js
+++ jsh.js
@@ -1,8 +1,28 @@
+// utilitities implemented by me
+// map - takes an array and function references and gives back a new array based on the function references
+const map = function (array, method) {
+  const modifiedArray = [];
+  for (let index = 0; index < array.length; index++) {
+    modifiedArray.push(method(array[index]));
+  }
+  return modifiedArray;
+};
+
+const filter = function (array, method) {
+  const modifiedArray = [];
+  for (let index = 0; index < array.length; index++) {
+    if (method(array[index])) {
+      modifiedArray.push(array[index]);
+    }
+  }
+  return modifiedArray;
+};
+
 let userName = "root";
 let pwdRegistery = [0, 0, 0];
 
 const rootFileSystem = [["root", [
-  ["js", [["assignments", [["functions", []], ["recursion", []]]]]],
+  ["js", [["assignments", [["functions", []], ["recursion", []]]], "hello.js"]],
   ["downloads", []],
   ["documents", []],
   ["pictures", []],
@@ -21,6 +41,29 @@ const generatePwd = function () {
   return folders.join("/");
 };
 
+const getCurrentFileSystem = function () {
+  let currentFileSystem = rootFileSystem;
+  for (let index = 0; index < pwdRegistery.length; index++) {
+    const location = pwdRegistery[index];
+    currentFileSystem = currentFileSystem[location][1];
+  }
+  return currentFileSystem;
+};
+
+const stripContents = function (content) {
+  return "./" + (Array.isArray(content) ? content[0] + "/" : content);
+};
+
+const getFileSystemContents = function () {
+  const currentDirectory = getCurrentFileSystem();
+  const contents = map(currentDirectory, stripContents);
+  return contents.join("\t");
+};
+
+const findFolderIndex = function (folderName) {
+  const currentDirectory = getCurrentFileSystem();
+};
+
 const cd = function (folderName) {
   if (folderName === "..") {
     if (pwdRegistery.length === 1) {
@@ -32,13 +75,16 @@ const cd = function (folderName) {
   pwdRegistery.push(0);
 };
 
-const ls = function () {};
+const ls = function () {
+  const contents = getFileSystemContents();
+  console.log(contents);
+};
 
 const functions = [cd, ls];
 const functionsRegistery = ["cd", "ls"];
 
 const userInput = function (userName, path) {
-  const leading = userName + "@" + path + ">";
+  const leading = "\n" + userName + "@" + path + ">";
   return prompt(leading).trim().split(" ");
 };
 
