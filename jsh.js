07cc2f43a96d77d54fc0a70a79c266eb165ee3f1
jayanth-14 <jayanthpetchetii@gmail.com>
2025-11-05 20:53:06 +0530

    implementing filesystem and cd backwards

---
+++ jsh.js
@@ -0,0 +1,64 @@
+let userName = "root";
+let pwdRegistery = [0, 0, 0];
+
+const rootFileSystem = [["root", [
+  ["js", [["assignments", [["functions", []], ["recursion", []]]]]],
+  ["downloads", []],
+  ["documents", []],
+  ["pictures", []],
+]]];
+const generatePwd = function () {
+  const location = pwdRegistery;
+  const folders = [];
+  let currentFileSystem = rootFileSystem;
+  for (let index = 0; index < location.length; index++) {
+    const currentFolderIndex = location[index];
+    const currentDirectory = currentFileSystem[currentFolderIndex];
+    folders.push(currentDirectory[0]);
+    currentFileSystem = currentDirectory[1];
+  }
+  return folders.join("/");
+};
+
+const cd = function (folderName) {
+  console.log("executing cd : ", folderName);
+  if (folderName === "..") {
+    return pwdRegistery.pop();
+  }
+  pwdRegistery.push(0);
+};
+
+const ls = function () {};
+
+const functions = [cd, ls];
+
+const functionsRegistery = ["cd", "ls"];
+
+const userInput = function (userName, path) {
+  const leading = userName + "@" + path + ">";
+  return prompt(leading).split(" ");
+};
+
+const getCommandReference = function (commandName) {
+  for (let index = 0; index < functionsRegistery.length; index++) {
+    if (functionsRegistery[index] === commandName) {
+      return functions[index];
+    }
+  }
+};
+
+const executeCommand = function (commandInfo) {
+  const commandName = commandInfo[0];
+  const command = getCommandReference(commandName);
+  command(commandInfo[1]);
+};
+
+const start = function () {
+  while (true) {
+    const pwd = generatePwd();
+    const commandInfo = userInput(userName, pwd);
+    executeCommand(commandInfo)
+  }
+};
+
+start();
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-# Basic shell simulation 
+# Basic shell simulation
+
+# thoughts
+files will be in string format
+  Each directory should be in a format of array - ["directory-name", ["file1", "file2", ["sub-directory-name", ["subfile1"]]]];
