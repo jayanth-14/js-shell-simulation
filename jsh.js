3c137db179069348c8e9af01fc34bfe10f8e9dcf
jayanth-14 <jayanthpetchetii@gmail.com>
2025-11-09 16:48:04 +0530

    removed space

--- jsh.js
+++ jsh.js
@@ -219,7 +219,6 @@ const listInLongFormat = data => {
       ? sizeOfDirectory(item) + " items"
       : item[1].join("").length + " chars";
     result.push(`${padColumn(type, 20)} ${padColumn(owner, 6)} ${padColumn(size, 10)} ${colorizeFolder(item)}`);
-    
   }
 
   return result.join("\n");
